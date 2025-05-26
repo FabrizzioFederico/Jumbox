3767703f31d6f6d2005b028647f984815d347612
martinaruizdev <martinaruizdev@gmail.com>
2025-05-26 17:45:11 -0300

    Cambios menores

--- Jumbox/src/GUI/VistaLogin.java
+++ Jumbox/src/GUI/VistaLogin.java
@@ -17,12 +17,12 @@ import java.awt.event.ActionEvent;
 import javax.swing.DropMode;
 import javax.swing.ImageIcon;
 
-public class PantallaPrincipal extends JFrame {
+public class VistaLogin extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField inpUsuario;
+	private JTextField inpContrasenia;
 
 	/**
 	 * Launch the application.
@@ -31,7 +31,7 @@ public class PantallaPrincipal extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PantallaPrincipal frame = new PantallaPrincipal();
+					VistaLogin frame = new VistaLogin();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -43,7 +43,7 @@ public class PantallaPrincipal extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public PantallaPrincipal() {
+	public VistaLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 558, 367);
 		contentPane = new JPanel();
@@ -65,7 +65,7 @@ public class PantallaPrincipal extends JFrame {
 		panel.setLayout(null);
 		
 		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setIcon(new ImageIcon(PantallaPrincipal.class.getResource("/img/logo2.png")));
+		lblNewLabel_1.setIcon(new ImageIcon(VistaLogin.class.getResource("/img/logo2.png")));
 		lblNewLabel_1.setBounds(-10, 0, 253, 328);
 		panel.add(lblNewLabel_1);
 		
@@ -86,35 +86,35 @@ public class PantallaPrincipal extends JFrame {
 		lblContrasea.setBounds(282, 166, 79, 29);
 		contentPane.add(lblContrasea);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Montserrat", Font.PLAIN, 11));
-		textField.setForeground(new Color(0, 0, 0));
-		textField.setBackground(new Color(255, 255, 255));
-		textField.setBounds(282, 134, 218, 25);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		inpUsuario = new JTextField();
+		inpUsuario.setFont(new Font("Montserrat", Font.PLAIN, 11));
+		inpUsuario.setForeground(new Color(0, 0, 0));
+		inpUsuario.setBackground(new Color(255, 255, 255));
+		inpUsuario.setBounds(282, 134, 218, 25);
+		contentPane.add(inpUsuario);
+		inpUsuario.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Montserrat", Font.PLAIN, 11));
-		textField_1.setBounds(282, 192, 218, 25);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		inpContrasenia = new JTextField();
+		inpContrasenia.setFont(new Font("Montserrat", Font.PLAIN, 11));
+		inpContrasenia.setBounds(282, 192, 218, 25);
+		contentPane.add(inpContrasenia);
+		inpContrasenia.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Ingresar");
-		btnNewButton.setForeground(new Color(255, 255, 255));
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnIngresar = new JButton("Ingresar");
+		btnIngresar.setForeground(new Color(255, 255, 255));
+		btnIngresar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBackground(new Color(63, 192, 108));
-		btnNewButton.setFont(new Font("Montserrat", Font.PLAIN, 13));
-		btnNewButton.setBounds(282, 242, 92, 29);
-		contentPane.add(btnNewButton);
+		btnIngresar.setBackground(new Color(63, 192, 108));
+		btnIngresar.setFont(new Font("Montserrat", Font.PLAIN, 13));
+		btnIngresar.setBounds(282, 242, 92, 29);
+		contentPane.add(btnIngresar);
 		
-		JButton btnNewButton_1 = new JButton("Registrarme");
-		btnNewButton_1.setBackground(new Color(255, 255, 255));
-		btnNewButton_1.setFont(new Font("Montserrat", Font.PLAIN, 12));
-		btnNewButton_1.setBounds(384, 242, 114, 29);
-		contentPane.add(btnNewButton_1);
+		JButton btnRegistrar = new JButton("Registrarme");
+		btnRegistrar.setBackground(new Color(255, 255, 255));
+		btnRegistrar.setFont(new Font("Montserrat", Font.PLAIN, 12));
+		btnRegistrar.setBounds(384, 242, 114, 29);
+		contentPane.add(btnRegistrar);
 	}
 }
