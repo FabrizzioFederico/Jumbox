ff3e2d88b92d7ad3148291aebb591fd9a137db1a
FabrizzioFederico <Fabrizziofederico@hotmail.com>
2025-06-01 22:55:19 -0300

    Modificación en el Label Error del Login

--- Jumbox/src/GUI/VistaLogin.java
+++ Jumbox/src/GUI/VistaLogin.java
@@ -59,13 +59,13 @@ public class VistaLogin extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblTitulo = new JLabel("Iniciar Sesión");
-		lblTitulo.setFont(new Font("Montserrat", Font.PLAIN, 20));
 		lblTitulo.setBounds(282, 65, 167, 29);
+		lblTitulo.setFont(new Font("Montserrat", Font.PLAIN, 20));
 		contentPane.add(lblTitulo);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(51, 153, 102));
 		panel.setBounds(0, 0, 244, 328);
+		panel.setBackground(new Color(51, 153, 102));
 		contentPane.add(panel);
 		panel.setLayout(null);
 
@@ -75,51 +75,49 @@ public class VistaLogin extends JFrame {
         panel.add(lblLogo);
 		
 		JLabel lblBienvenido = new JLabel("¡Bienvenido a Jumbox!");
+		lblBienvenido.setBounds(282, 22, 206, 30);
 		lblBienvenido.setBackground(new Color(54, 201, 102));
 		lblBienvenido.setForeground(new Color(51, 153, 102));
 		lblBienvenido.setFont(new Font("Montserrat", Font.PLAIN, 16));
-		lblBienvenido.setBounds(282, 22, 206, 30);
 		contentPane.add(lblBienvenido);
 				
 		JLabel lblUsuario = new JLabel("Nombre de usuario");
+		lblUsuario.setBounds(282, 105, 167, 29);
 	    lblUsuario.setFont(new Font("Montserrat", Font.PLAIN, 12));
-	    lblUsuario.setBounds(282, 105, 167, 29);
 	    contentPane.add(lblUsuario);		
 		
         JLabel lblContrasenia = new JLabel("Contraseña");
-        lblContrasenia.setFont(new Font("Montserrat", Font.PLAIN, 12));
         lblContrasenia.setBounds(282, 166, 79, 29);
+        lblContrasenia.setFont(new Font("Montserrat", Font.PLAIN, 12));
         contentPane.add(lblContrasenia);
         
         inpUsuario = new JTextField();
-        inpUsuario.setFont(new Font("Montserrat", Font.PLAIN, 11));
         inpUsuario.setBounds(282, 134, 218, 25);
+        inpUsuario.setFont(new Font("Montserrat", Font.PLAIN, 11));
         contentPane.add(inpUsuario);
         inpUsuario.setColumns(10);
         
         inpContrasenia = new JPasswordField(); 
-        inpContrasenia.setFont(new Font("Montserrat", Font.PLAIN, 11));
         inpContrasenia.setBounds(282, 192, 218, 25);
+        inpContrasenia.setFont(new Font("Montserrat", Font.PLAIN, 11));
         contentPane.add(inpContrasenia);
-        
+		
 		JLabel lblError = new JLabel("");
 		lblError.setForeground(Color.RED);
-		lblError.setBounds(40, 355, 265, 32);
+		lblError.setBounds(282, 222, 92, 19);
+		lblError.setFont(new Font("Montserrat", Font.PLAIN, 11));
 		contentPane.add(lblError);
+
 		
 		btnIngresar = new JButton("Ingresar");
+		btnIngresar.setBounds(282, 242, 92, 29);
 		btnIngresar.setForeground(new Color(255, 255, 255));
 		btnIngresar.setBackground(new Color(63, 192, 108));
 		btnIngresar.setFont(new Font("Montserrat", Font.PLAIN, 13));
-		btnIngresar.setBounds(282, 242, 92, 29);
 		btnIngresar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String usuario = inpUsuario.getText();
                 String contrasenia = new String(inpContrasenia.getPassword());
-                if(usuario.isEmpty() || contrasenia.isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "Error. Debe completar los campos");
-                    return;
-                }
                 
 				Usuario logueado = Usuario.login(usuario, contrasenia);
 				
@@ -151,11 +149,9 @@ public class VistaLogin extends JFrame {
 		contentPane.add(btnIngresar);
 		
 		btnRegistrar = new JButton("Registrarme");
+		btnRegistrar.setBounds(384, 242, 114, 29);
 		btnRegistrar.setBackground(new Color(255, 255, 255));
 		btnRegistrar.setFont(new Font("Montserrat", Font.PLAIN, 12));
-		btnRegistrar.setBounds(384, 242, 114, 29);
 		contentPane.add(btnRegistrar);
-
-
 	}
 }
